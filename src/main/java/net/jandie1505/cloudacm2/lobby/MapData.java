a2db7fbc2ea46a34e5dc5d93ce6bf74270e05ea8
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-07-28 10:36:28 +0200

    commented out central island because it's currently not working

--- src/main/java/net/jandie1505/cloudacm2/lobby/MapData.java
+++ src/main/java/net/jandie1505/cloudacm2/lobby/MapData.java
@@ -18,14 +18,14 @@ public class MapData {
         );
 
         TDMMaps = List.of(
-                new MapData(100, 4, "Central Island"),
+                //new MapData(100, 4, "Central Island"),
                 new MapData(201, 4, "School"),
                 new MapData(301, 4, "Modern Village"),
                 new MapData(401, 4, "Old village")
         );
 
         CTFMaps = List.of(
-                new MapData(100, 5, "Central Island"),
+                //new MapData(100, 5, "Central Island"),
                 new MapData(200, 5, "School"),
                 new MapData(300, 5, "Modern Village"),
                 new MapData(400, 5, "Old village")
